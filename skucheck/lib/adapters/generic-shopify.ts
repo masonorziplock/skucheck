13d4cf8d2bdd6f119d797fd7aba3f51aaed55442
masonorziplock <sctmoss@gmail.com>
2026-06-03 08:31:07 -0400

    Delete skucheck directory

--- skucheck/lib/adapters/generic-shopify.ts
+++
@@ -1,9 +0,0 @@
-import type { StoreAdapter } from "./types";
-import { searchPublicShopifyStore } from "./shopify-public";
-
-export function createShopifyAdapter(storeId: string, baseUrl: string): StoreAdapter {
-  return {
-    storeId,
-    search: async (sku) => searchPublicShopifyStore({ storeId, baseUrl, sku }),
-  };
-}
--- skucheck/lib/app-config.ts
+++
@@ -1,9 +0,0 @@
-export const APP_VERSION = "2.6.0";
-export const SEARCH_CACHE_TTL_SECONDS = 10 * 60;
-export const SEARCH_RATE_LIMIT_WINDOW_MS = 60 * 1000;
-export const SEARCH_RATE_LIMIT_MAX_REQUESTS = 20;
-export const ADAPTER_TIMEOUT_MS = 8000;
-
-export function getRateLimitLabel() {
-  return `${SEARCH_RATE_LIMIT_MAX_REQUESTS} searches / ${Math.round(SEARCH_RATE_LIMIT_WINDOW_MS / 1000)} sec`;
-}
